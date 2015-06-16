e9d8529de4de8d9acc5c674c1db87f4eb02b2399
mozotom <mozotom@yahoo.com>
2015-06-15 20:41:06 -0500

    Initial commit - dug out old source

---
+++ resizeDir.bat
@@ -0,0 +1,4 @@
+rem java -Xmx1536m -classpath release\MosaicMaker.jar com.tomaszmozolewski.jpegtools.ImageToolkit ScaledCutInstanceDir "F:/Photos" "C:/Photos_134x116" 134 116
+
+C:\j2sdk1.4.2_07\bin\java -Xmx1536m -verbosegc -classpath release\MosaicMaker.jar com.tomaszmozolewski.jpegtools.ImageToolkit ScaledCutInstanceDir %1 %2 %3 %4
+
